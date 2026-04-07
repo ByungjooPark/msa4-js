b4121c2e81bdda3b3c5c765371cb6dc690c3e76d
ByungjooPark <milkpbj23@gmail.com>
2026-04-07 16:48:17 +0900

    260407 [이론 + 실습] 네이밍기법, 루프

---
+++ tng/06_for.js
@@ -0,0 +1,61 @@
+// 아래처럼 별을 찍어주세요.
+// 예시)
+// *****
+// *****
+// *****
+// *****
+// *****
+for(let i = 0; i < 5; i++) {
+  console.log('*****');
+}
+
+for(let i = 0; i < 5; i++) {
+  let star = '';
+  for(let z = 0; z < 5; z++) {
+    star += '*';
+  }
+  console.log(star);
+}
+
+// 아래처럼 별을 찍어주세요.
+// 예시)
+// *
+// **
+// ***
+// ****
+// *****
+for(let i = 0; i < 5; i++) {
+  let star = '';
+  for(let z = 0; z <= i; z++) {
+    star += '*';
+  }
+  console.log(star);
+}
+
+// ★ 반장님 방법 ★
+let ast = '';
+for(let i = 1; i < 6; i++) {
+  ast += '*';
+  console.log(ast);
+}
+
+// 아래처럼 별을 찍어주세요.
+// 예시)
+//     *
+//    **
+//   ***
+//  ****
+// *****
+for(let i = 0; i < 5; i++) {
+  let space = '';
+  for(let z = 4; z > i; z--) {
+    space += ' ';
+  }
+  
+  let star = '';
+  for(let y = 0; y <= i; y++) {
+    star += '*';
+  }
+  
+  console.log(`${space}${star}`);
+}
